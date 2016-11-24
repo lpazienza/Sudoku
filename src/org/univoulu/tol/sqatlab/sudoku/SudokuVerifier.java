5295ae4deb6b9d0ed4aec00bb77d476bb2cf0cfe
lpazienza <l.pazienza1@studenti.uniba.it>
2016-11-24 17:07:39 +0100

    Vitino

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -3,7 +3,75 @@ package org.univoulu.tol.sqatlab.sudoku;
 public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
-		// returns 1 if the candidate solution is correct
-		return 0;
+		return r1(candidateSolution);
+	}
+	
+	public int r1(String candidato){
+		int risultato = 0;
+		int[] sudokuArray = new int[candidato.length()];
+		
+		for(int i = 0;i<candidato.length();i++){
+			sudokuArray[i] = Character.getNumericValue(candidato.charAt(i));
+			if(sudokuArray[i] < 1){
+				risultato = -1;
+			}
+		}
+		return risultato;
+	}
+	
+	public boolean differentDigits(String numeri){
+		boolean risultato = true;
+		if(numeri.length()!=9){
+			risultato = false;
+		}
+		
+		for(int i=0; i<numeri.length(); i++){
+			for(int j=i+1;j<numeri.length();j++){
+				if(numeri.charAt(i) == (numeri.charAt(j))){
+					risultato=false;
+				}
+			}
+		}
+		return risultato;
+	}
+	
+	public int r2(String candidato){
+		int risultato = 0;
+		String holder="";
+		
+		for(int j=0;j<3;j++){
+			for(int i=0; i<9;i++){
+				for(int k=0; k<3; k++){
+					if(j==1){
+						k+=3;
+					}
+					if(j==2){
+						k+=6;
+					}
+					holder+=candidato.charAt(k);
+				}
+				if(i==2 || i==5 || i==8){
+					if(!differentDigits(holder)){
+						risultato = -1;
+					}
+				}
+			}
+		}
+		return risultato;
+	}
+	
+	public int r3(String candidato){
+		int risultato = 0;
+		int min=0;
+		int max=9;
+		
+		for(int i=0; i<9;i++){
+			if(!differentDigits(candidato.substring(min, max))){
+				risultato = -1;
+			}
+			min = max;
+			max +=9;
+		}
+		return risultato;
 	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -2,13 +2,52 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSudokuVerifier {
+	SudokuVerifier mySudoku;
+	
+	@Before
+	public void setUp(){
+		mySudoku = new SudokuVerifier();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void r1Respected() {
+		//act
+		int valid = mySudoku.r1("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		//assert
+		assertEquals(0,valid);
+	}
+	@Test
+	public void differentDigitsIsTrue(){
+		assertTrue(mySudoku.differentDigits("256438971"));
+	}
+	
+	@Test
+	public void differentDigitsIsFalse(){
+		assertFalse(mySudoku.differentDigits("112345678"));
+	}
+	
+	@Test
+	public void r3Respected(){
+		//act
+		int valid = mySudoku.r3("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		//assert
+		assertEquals(0,valid);
+		
+		
+	}
+	
+	@Test
+	public void r2Respected(){
+		//act
+		int valid = mySudoku.r2("417369825632158947958724316825437169791586432346912758289643571573291684164875293");
+		//assert
+		assertEquals(0,valid);
+		
+		
 	}
 
 }
